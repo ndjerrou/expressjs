4cd3a5f75e119d7b867bf2bfdeb32b4eb6f8dc8c
ndjerrou <n.djerrou@gmail.com>
2022-01-04 10:23:55 +0100

    multiple get routes

--- expressjs/src/server.js
+++ expressjs/src/server.js
@@ -2,5 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//endpoints = routes
+app.get("/", (req, res) => {
+  res.send("Welcome to my homepage");
+});
+
+app.get("/temperature", (req, res) => {
+  res.send("Le température à Paris est de 2 degrés...");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Je suis un développeur et je m'appelle Nissim");
+});
+
 const port = 3000;
 app.listen(port, () => console.log(`Listenning on port ${port}...`));
