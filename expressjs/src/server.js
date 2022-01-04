de95461046e8de5e3d482bb5811fb28049723d30
ndjerrou <n.djerrou@gmail.com>
2022-01-04 11:34:22 +0100

    static routes

--- expressjs/src/server.js
+++ expressjs/src/server.js
@@ -5,13 +5,8 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
-// requête entrante GET / ==> index.html
 app.use(express.static(publicDirectoryPath));
 
-//endpoints = routes
-app.get("/", (req, res) => {
-  res.send("/public/index.html");
-});
-
 const port = 3000;
+
 app.listen(port, () => console.log(`Listenning on port ${port}...`));
